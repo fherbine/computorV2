82cba17a0a50f810fba67439e1e2175584107ac4
fherbine <felix.herbinet@yahoo.com>
2019-10-19 19:22:32 +0200

    expr cast + basic expr calculation

--- controllers/parser.py
+++ controllers/parser.py
@@ -1,4 +1,63 @@
 from sly import Parser
 
+from controllers.lexer import BcLexer
+from controllers.ft_math import *
+
+sanitize_result = lambda a: int(a) if float(a).is_integer() else float(a)
+
 class BcParser(Parser):
-    pass
+    tokens = BcLexer.tokens
+
+    def __init__(self):
+        """Define functions and variables dicts.
+
+        self.variables struct:
+        {<VAR1_CAP>: <value>, <VAR2_CAP>, ...}
+
+        self.functions struct:
+        {
+            <FUNC1_CAP>: BcFunc(),
+            <FUNC2_CAP>: BcFunc(),
+            ...
+        }
+        """
+        self.variables = {}
+        self.functions = {}
+
+    # regular expr operations
+    @_('expr ADD expr')
+    def expr(self, parsed):
+        return parsed.expr0 + parsed.expr1
+
+    @_('expr MINUS expr')
+    def expr(self, parsed):
+        return parsed.expr0 - parsed.expr1
+
+    @_('expr TIMES expr')
+    def expr(self, parsed):
+        return parsed.expr0 * parsed.expr1
+
+    @_('expr DIVIDE expr')
+    def expr(self, parsed):
+        return sanitize_result(parsed.expr0 / parsed.expr1)
+
+    @_('expr MODULO expr')
+    def expr(self, parsed):
+        return parsed.expr0 % parsed.expr1
+
+    @_('expr INTDIV expr')
+    def expr(self, parsed):
+        return parsed.expr0 // parsed.expr1
+
+    @_('expr POWER expr')
+    def expr(self, parsed):
+        return ft_power(parsed.expr0, parsed.expr1)
+
+    # regular expr
+    @_('NUMBER')
+    def expr(self, parsed):
+        return parsed.NUMBER
+
+    @_('IMAG')
+    def expr(self, parsed):
+        return Complex(0, 1)
