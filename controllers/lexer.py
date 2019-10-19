a3d432a9fac381b7fc37fb038a88248c99244406
fherbine <felix.herbinet@yahoo.com>
2019-10-19 12:23:34 +0200

    Working lexer

--- controllers/lexer.py
+++ controllers/lexer.py
@@ -1,4 +1,35 @@
 from sly import Lexer
 
 class BcLexer(Lexer):
-    pass
+    tokens = { ADD, MINUS, TIMES, DIVIDE, ASSIGN, ID, IMAG,
+               LPAREN, RPAREN, LBRCK, RBRCK, MODULO, QMARK,
+               COMA, NUMBER }
+
+    ignore = ' \t'
+
+    ADD = r'\+'
+    MINUS = r'-'
+    TIMES = r'\*'
+    DIVIDE = r'/'
+    ASSIGN = r'='
+    IMAG = r'[iI]'
+    ID = r'[A-Za-z]+'
+    LPAREN = r'\('
+    RPAREN = r'\)'
+    LBRCK = r'\['
+    RBRCK = r'\]'
+    MODULO = r'%'
+    QMARK = '\?'
+    COMA = r','
+    NUMBER = r'(?:[0-9]*\.)?[0-9]+'
+
+    def NUMBER(self, token):
+        if float(token.value).is_integer():
+            token.value = int(token.value)
+        else:
+            token.value = float(token.value)
+        return token
+
+    def error(self, token):
+        raise SyntaxError('Unknown character `%s`' % token.value[0])
+        self.index += 1
