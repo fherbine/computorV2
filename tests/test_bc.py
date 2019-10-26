5ad58b5045a1c8820865e0c412d030a717310c93
fherbine <felix.herbinet@yahoo.com>
2019-10-26 13:32:08 +0200

    Variables assignation tests

--- tests/test_bc.py
+++ tests/test_bc.py
@@ -61,9 +61,47 @@ def test_composed_basic_operators_2():
     assert get_line_result('(((21%(2+20))) * 2)/1 + (0 * 42)//1 = ?') == 42
 
 #======================== variable assignement ===============================
+
+def test_simple_assignation():
+    assert get_line_result('a = 42') == 42
+
+def test_simple_assignation_with_operation():
+    assert get_line_result('a = (((21%(2+20))) * 2)/1 + (0 * 42)//1') == 42
+
+def test_simple_assignation_with_variable():
+    res = bc_repl('a = 42', 'key = a')
+    assert res == 42
+
+def test_simple_assignation_with_variables_calculation():
+    res = bc_repl('a = 42','one=1', 'zero = one - 1', 'key = (a + zero) * one')
+    assert res == 42
+
+def test_simple_assignation_with_func():
+    res = bc_repl('a(x) = x', 'key = a(42)')
+    assert res == 42
+
+def test_simple_assignation_with_func_var_arg():
+    res = bc_repl('a(x) = x * 2', 'moscow = 21', 'key = a(moscow)')
+    assert res == 42
+
+def test_simple_assignation_with_func_func_arg():
+    res = bc_repl('a(x) = x * 2', 'f(x) = x', 'key = a(f(21))')
+    assert res == 42
+
+def test_complex_assignation():
+    res = bc_repl(
+        'a(x) = x * 2',
+        'moscow = 21',
+        'useless(toto) = toto',
+        'two = 2',
+        'key = (useless(a(moscow)) + a(0) + a(useless(moscow))) * two // 4',
+    )
+    assert res == 42
+
 #======================== variable calculation ===============================
 #======================== functions assignement ==============================
 #======================== function calculation ===============================
+#======================== Vars & funcs rewrite ===============================
 #======================== Polynomials ========================================
 #------------------------ BONUS ==============================================
 
